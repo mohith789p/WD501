3bc4fa518275f5f02d47ce96630a4d27ae10d0f3
Potnuru Mohith <mohith321p@gmail.com>
2025-08-31 12:29:09 +0530

    streams concept

--- hello-node/index.js
+++ hello-node/index.js
@@ -1,10 +1,9 @@
+const http = require("http");
 const fs = require("fs");
 
-fs.rename("sample.txt", "test.txt", (err) => {
-  if (err) throw err;
-  console.log("File name updated!");
+const server = http.createServer((req, res) => {
+  const stream = fs.createReadStream("sample.txt");
+  stream.pipe(res);
 });
-fs.unlink("test.txt", (err) => {
-  if (err) throw err;
-  console.log("File test.txt deleted successfully!");
-});
+
+server.listen(8080);
