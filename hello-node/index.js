31ae24b1d9e9f5784726034daf187aac28aa1042
Potnuru Mohith <mohith321p@gmail.com>
2025-09-05 13:48:23 +0530

    event loop / asynchronus

--- hello-node/index.js
+++ hello-node/index.js
@@ -1,54 +1,7 @@
-const http = require("http");
-const fs = require("fs");
-const minimist = require("minimist");
+console.log("Hello 1");
 
-let args = minimist(process.argv.slice(2), {
-  alias: {
-    p: "port",
-  },
-  default: {
-    port: 3000,
-  },
-});
-
-let homeContent = "";
-let projectContent = "";
-let registrationContent = "";
-
-fs.readFile("home.html", (err, home) => {
-  if (err) throw err;
-  homeContent = home;
-});
-
-fs.readFile("project.html", (err, project) => {
-  if (err) throw err;
-  projectContent = project;
-});
-
-fs.readFile("registration.html", (err, registration) => {
-  if (err) throw err;
-  registrationContent = registration;
-});
-
-http
-  .createServer((req, res) => {
-    let url = req.url;
-    res.writeHeader(200, { "Content-Type": "text/html" });
-    switch (url) {
-      case "/project":
-        res.write(projectContent);
-        res.end();
-        break;
-      case "/registration":
-        res.write(registrationContent);
-        res.end();
-        break;
-      default:
-        res.write(homeContent);
-        res.end();
-        break;
-    }
-  })
-  .listen(args.port, () => {
-    console.log(`server is listening on http://localhost:${args.port}`);
-  });
+// Print message after 100 millisecond
+setTimeout(() => {
+  console.log("Hello 2");
+}, 100);
+console.log("Hello 3");
